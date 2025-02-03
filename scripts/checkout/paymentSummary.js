afdf76aa62188d9715c610cd9e8b69cff2effe70
Loopfelloff <abhiyanregmi@gmail.com>
2025-02-03 17:40:10 +0545

    saving orders

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -1,6 +1,7 @@
 import {cart} from '../../data/cart.js';
 import {getProduct} from '../../data/products.js';
 import { getDeliveryOption } from '../../data/deliveryOptions.js';
+import {addOrder} from '../../data/orders.js';
 import formatCurrency from '../utils/money.js';
 export function renderPaymentSummary(){
     let productPriceCents =0;
@@ -45,11 +46,31 @@ export function renderPaymentSummary(){
             <div class="payment-summary-money">$${formatCurrency(totalCents)}</div>
           </div>
 
-          <button class="place-order-button button-primary">
+          <button class="place-order-button button-primary js-place-order">
             Place your order
           </button>
     `;
     document.querySelector('.js-payment-summary')
         .innerHTML = paymentSummaryHTML;
-    
+    document.querySelector('.js-place-order').addEventListener('click' , async ()=>{
+     try{
+        const response = await  fetch(`https://supersimplebackend.dev/orders` , {
+              method: 'POST',
+              headers:{
+                'Content-Type' : 'application/json'
+              },
+              body: JSON.stringify({
+                cart : cart
+              })
+             });  
+            const order =  await response.json();
+            console.log(order);  
+            addOrder(order);  
+     }
+     catch(error)
+     {
+      console.log('error uploading cart');
+     }  
+     window.location.href = 'orders.html';   
+    });
 }
