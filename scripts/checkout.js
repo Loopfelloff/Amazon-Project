ca7752a8944d2e02176daa5cb3021b2632267829
Loopfelloff <abhiyanregmi@gmail.com>
2025-02-03 16:48:19 +0545

    convert from promises to async await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,16 +2,33 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProductsFetch} from "../data/products.js";
 import { loadCart } from "../data/cart.js";
- 
-Promise.all([
-   
-    loadProductsFetch(),
-     new Promise((resolve)=>{
-        loadCart(()=>{resolve('cart Loaded');});
-    })
-    
-]).then((message)=>{
-    console.log(message);
-    renderOrderSummary();
+
+
+async function loadPage(){; 
+    let [firstAwait , secondAwait] = await Promise.all([
+        loadProductsFetch(),
+        new Promise((resolve)=>{
+            loadCart(()=>{resolve('cart Loaded');});
+       })
+    ]);
+    console.log(firstAwait);
+    console.log(secondAwait);
+   renderOrderSummary();
     renderPaymentSummary();
-});
+   return 'nextstep';
+ }
+ loadPage().then(message=>{
+   console.log(message);
+ });
+// Promise.all([
+   
+//     loadProductsFetch(),
+//      new Promise((resolve)=>{
+//         loadCart(()=>{resolve('cart Loaded');});
+//     })
+    
+// ]).then((message)=>{
+//     console.log(message);
+//     renderOrderSummary();
+//     renderPaymentSummary();
+// });
