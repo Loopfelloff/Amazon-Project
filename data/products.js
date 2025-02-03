cfc142981fea04d908ded31fb9f6546f3d1f668c
Loopfelloff <abhiyanregmi@gmail.com>
2025-02-03 15:16:06 +0545

    added promises and fetch instead of XMLHTTPReqeust

--- data/products.js
+++ data/products.js
@@ -52,11 +52,40 @@ class Clothing extends Product{
 }
 
 export let products = [];
+
+export function loadProductsFetch(){
+ const promise =  fetch(
+  'https://supersimplebackend.dev/products'
+  ).
+  then((response)=>{
+   return response.json(); //response.json() returns a promise which is async
+  })
+  .then((productsData)=>{
+    console.log(productsData); // here the response text that we used to get as array is now a array , it parses it to
+    // array.
+    products = productsData.map((productDetails)=>{
+      if(productDetails.type === 'clothing')
+      {
+        return new Clothing(productDetails);
+      }
+    return new Product(productDetails);
+    });
+    console.log('product load');
+   
+  });
+  return promise;
+}
+loadProductsFetch().then(()=>{
+console.log('next step');
+});
+
 export function loadProducts(fun){
 const xhr = new XMLHttpRequest();
+
 xhr.open('GET' , 'https://supersimplebackend.dev/products' , true);
 xhr.send();
 xhr.addEventListener('load' , ()=>{
+  
 products = JSON.parse(xhr.response).map((productDetails)=>{
   if(productDetails.type === 'clothing')
   {
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,15 +1,11 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import { loadProducts} from "../data/products.js";
+import { loadProductsFetch} from "../data/products.js";
 import { loadCart } from "../data/cart.js";
  
 Promise.all([
-    new Promise((resolve)=>{
-
-        loadProducts(()=>{
-         resolve('products loaded');
-        });
-     }),
+   
+    loadProductsFetch(),
      new Promise((resolve)=>{
         loadCart(()=>{resolve('cart Loaded');});
     })
@@ -19,24 +15,3 @@ Promise.all([
     renderOrderSummary();
     renderPaymentSummary();
 });
-
-// new Promise((resolve)=>{
-
-//    loadProducts(()=>{
-//     resolve('products loaded');
-//    });
-// }).then(message => {
-//     console.log(message);
-
-//     return new Promise((resolve)=>{
-//         loadCart(()=>{resolve('cart Loaded');});
-       
-//     });
-//    })
-//     .then(message =>{
-//         console.log(message);
-//         renderOrderSummary();
-//         renderPaymentSummary();
-//      });
-   
-
